208609ca48a298508c8d8ed5eead887926b72630
narkeekran <narkeekran@googlemail.com>
2016-09-26 20:01:40 +0100

    added more error checking for duff subreddits

--- remy.py
+++ remy.py
@@ -41,17 +41,20 @@ if __name__ == "__main__":
 							if (lower_message.find("hello") != -1) or (lower_message.find("hi") != -1):
 								slack_client.rtm_send_message(channel, "Hello <@{}> ;)".format(user_id))
 							if lower_message.find("/r/") != -1:
-								subreddit = re.findall('\/r\/\w+',lower_message)
-								sub = re.findall('\w+',subreddit[0])
-								top=random.randint(1,25)
-								try:
-									submissions = PRAW.get_subreddit(sub[1]).get_top(limit=top)
-									for item in submissions:
-										link=item.url
-									slack_client.rtm_send_message(channel, "<@{}>".format(user_id))
-									slack_client.api_call("chat.postMessage", channel=channel, text=link, as_user=True, unfurl_media=True) 
-								except praw.errors.InvalidSubreddit as e:
-									slack_client.api_call("chat.postMessage", channel=channel, text="I can't give you anything from a subreddit that doesn't exist", as_user=True, unfurl_media=True)
+                                                                subreddit = re.findall('\/r\/\w+',lower_message)
+                                                                sub = re.findall('\w+',subreddit[0])
+                                                                try:
+                                                                        top=random.randint(1,100)
+                                                                        submissions = PRAW.get_subreddit(sub[1]).get_top(limit=top)
+                                                                        for item in submissions:
+                                                                                link=item.url
+                                                                                title=item.title
+                                                                                print(item.url)
+                                                                                print(item.title)
+                                                                        slack_client.rtm_send_message(channel, "<@{}>".format(user_id))
+                                                                        slack_client.api_call("chat.postMessage", channel=channel, text='<' + link + '|' + title +'>', as_user=True, unfurl_media=True)
+                                                                except (praw.errors.InvalidSubreddit, praw.errors.Forbidden, praw.errors.NotFound):
+                                                                        slack_client.api_call("chat.postMessage", channel=channel, text="WTF subreddit is that?", as_user=True, unfurl_media=True)
 			time.sleep(READ_WEBSOCKET_DELAY)
 	else:
 		print("Connection failed, invalid token?")
