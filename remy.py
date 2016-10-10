41320ea559d9e849f3684a36b89780921916bcb1
narkeekran <narkeekran@googlemail.com>
2016-10-10 11:41:05 +0100

    cleaned up code into methods fixed a bug that caused remy to crash if the subreddit returns empty urls

--- remy.py
+++ remy.py
@@ -7,7 +7,7 @@ import random
 
 #set remy's bot id and API id as a variable
 
-SLACK_BOT_TOKEN = "TOKENBITCHES"
+SLACK_BOT_TOKEN = "xoxb-70506392340-UiZz2SrTpct1MgRtvpniaL9d"
 BOT_ID = "U22EWBJA0"
 PRAW = praw.Reddit(user_agent='Remy by /u/narkeekran')
 
@@ -20,8 +20,55 @@ slack_client = SlackClient(SLACK_BOT_TOKEN)
 #help messege
 help_message =  "I get images (mostly porn) from reddit! Try the following: \n  */r/$subreddit* _for a random post from the top 50_ \n *surprise me* _for a random image from a random NSFW subreddit_ \n"
 
+#Setting read delay
+READ_WEBSOCKET_DELAY = 1
 
-READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading
+
+
+def help(): 
+	slack_client.api_call("chat.postMessage", channel=channel, text=help_message.format(user_id), as_user=True, unfurl_media=True)
+
+def surprise_me(channel, user_id):
+	top=random.randint(1,100)
+	submissions = PRAW.get_subreddit("randnsfw").get_top(limit=top)
+	for item in submissions:
+		link=item.url
+		title=item.title
+	slack_client.rtm_send_message(channel, "<@{}>".format(user_id))
+	slack_client.api_call("chat.postMessage", channel=channel, text='<' + link + '|' + title +'>', as_user=True, unfurl_media=True)
+
+def top_post(channel, user_id, lower_message):
+	subreddit = re.findall('\/r\/\w+',lower_message)
+	sub = re.findall('\w+',subreddit[0])
+	link="" # set the link variable to nothing, this is so Remy can error out when a subreddit exists but doesn't give back links
+	try:
+		top=random.randint(1,100)
+		submissions = PRAW.get_subreddit(sub[1]).get_top(limit=top)
+#		print("assigned submissions")
+#		print(sub[1])
+#		print(subreddit)
+#		print(str(submissions))
+#		if str(submissions):
+#			print("nothing here")
+#			slack_client.api_call("chat.postMessage", channel=channel, text="There's nothing there", as_user=True, unfurl_media=True)
+#		else:
+		for item in submissions:
+#			print("loop")
+			link=item.url
+			title=item.title
+#			print(link)
+#			print(title)
+#		print("past for loop")
+#		print(link)
+		if link:
+			slack_client.rtm_send_message(channel, "<@{}>".format(user_id))
+			slack_client.api_call("chat.postMessage", channel=channel, text='<' + link + '|' + title +'>', as_user=True, unfurl_media=True)
+		else:
+			print("nothing here")
+			slack_client.api_call("chat.postMessage", channel=channel, text="There's nothing there", as_user=True, unfurl_media=True)
+
+	except (praw.errors.InvalidSubreddit, praw.errors.Forbidden, praw.errors.NotFound):
+		slack_client.api_call("chat.postMessage", channel=channel, text="WTF subreddit is that?", as_user=True, unfurl_media=True)
 
 
 if __name__ == "__main__":
@@ -41,43 +88,14 @@ if __name__ == "__main__":
 							channel=evt["channel"]
 							user_id=evt['user']
 							lower_message=message.lower()
-
 							if (lower_message.find("hello") != -1) or (lower_message.find("hi") != -1):
 								slack_client.rtm_send_message(channel, "Hello <@{}> ;)".format(user_id))
-
 							if lower_message.find("/r/") != -1:
-                                                                subreddit = re.findall('\/r\/\w+',lower_message)
-                                                                sub = re.findall('\w+',subreddit[0])
-                                                                try:
-                                                                        top=random.randint(1,100)
-                                                                        submissions = PRAW.get_subreddit(sub[1]).get_top(limit=top)
-                                                                        for item in submissions:
-                                                                                link=item.url
-                                                                                title=item.title
-                                                                        slack_client.rtm_send_message(channel, "<@{}>".format(user_id))
-                                                                        slack_client.api_call("chat.postMessage", channel=channel, text='<' + link + '|' + title +'>', as_user=True, unfurl_media=True)
-                                                                except (praw.errors.InvalidSubreddit, praw.errors.Forbidden, praw.errors.NotFound):
-                                                                        slack_client.api_call("chat.postMessage", channel=channel, text="WTF subreddit is that?", as_user=True, unfurl_media=True)
-
+								top_post(channel, user_id, lower_message)
 							if lower_message.find("surprise me") != -1:
-								top=random.randint(1,100)
-								submissions = PRAW.get_subreddit("randnsfw").get_top(limit=top)
-								for item in submissions:
-									link=item.url
-									title=item.title
-									print(item.url)
-									print(item.title)
-								slack_client.rtm_send_message(channel, "<@{}>".format(user_id))
-								slack_client.api_call("chat.postMessage", channel=channel, text='<' + link + '|' + title +'>', as_user=True, unfurl_media=True)
-
-							if lower_message.find("nap time") != -1:
-								time.sleep(300)
-
+								surprise_me(channel, user_id) 
 							if (lower_message.find("help") != -1) or (lower_message.find("halp") != -1) or (lower_message.find("i'm dumb") != -1):
-#								help_messege = ">>>Don't worry <@{}> I've got you \n you sack of shit"
-								slack_client.api_call("chat.postMessage", channel=channel, text=help_message.format(user_id), as_user=True, unfurl_media=True)
-
-
+								help()
 
 			time.sleep(READ_WEBSOCKET_DELAY)
 	else:
